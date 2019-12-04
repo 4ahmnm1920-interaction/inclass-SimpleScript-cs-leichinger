3621ec73fdd6187c60edfc26fdb241f8bee89530
leoeichinger <eichinger.l@gmx.at>
2019-12-04 11:35:54 +0100

    added a new script-- new method-- printed score and name in consle

---
+++ Assets/MyGame/Scripts/ScoreExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         SetScore(5, "Oasch");
+    }
+
+    public void SetScore(int score, string name)
+    {
+        int scoreValue = score;
+        string nameValue = name;
+        Debug.Log("score= " + scoreValue + " name= " + nameValue);
+    }
+}
